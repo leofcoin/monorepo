f6d4287bffb6bab4cee973503e6c93fb51deaccd
vandeurenglenn <vandeurenglenn@gmail.com>
2021-07-17 18:58:17 +0200

    addresses: update mainnet addresses

--- addresses/addresses/mainnet.js
+++ addresses/addresses/mainnet.js
@@ -1,13 +1,10 @@
 export default {
-  pools: {
-    0: '', // genesis
-    1000: '', // ARTX 1000
-    2000: '' // ARTX 2000
-  },
-  cards: {
-    0: '', // genesis
-    1000: '', // ARTX 1000
-    2000: '' // ARTX 2000
-  },
-  token: ''
+  "token": "0x897abf83f0c44b86cb67ec56c006c00d56659517",
+  "exchange": "0x2E8831f334B5362e5da01F083d2AD3D48e1835dE",
+  "factory": "0xcD83cf04Db8989fF54F208Bec3Bc3203431648a2",
+  "cards": {
+    "genesis": "0x5a654e6ae9d33fbf9b1295dfc3799007302527ca",
+    "artx1000": "0xd539b37fd96899d7850b16c73cba62e82be1118f",
+    "artx2000": "0xd733e21ea44083eee00811954db5ae3ab762b3f1"
+  }
 }
