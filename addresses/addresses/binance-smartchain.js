70de155eaf13cf6d198fd3392f92f76494ef0cd3
vandeurenglenn <vandeurenglenn@gmail.com>
2021-10-17 17:39:41 +0200

    Update addresses

---
+++ addresses/addresses/binance-smartchain.js
@@ -0,0 +1,7 @@
+export default {
+    "artonline": "0x535e67270f4FEb15BFFbFE86FEE308b81799a7a5",
+    "platform": "0x9D0F9765c2e912088b682DA9eaf7439a9440a6e4",
+    "presale": "undefined",
+    "proxy": "undefined",
+    "platformProxy": "undefined"
+    }
