f2a90da67f08bb62a0b86d9148872c367979451b
vandeurenglenn <vandeurenglenn@gmail.com>
2021-11-05 17:31:46 +0100

    contracts: add SafeArtOnline

---
+++ contracts/token/utils/SafeArtOnline.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "contracts/token/interfaces/IArtOnline.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+
+library SafeArtOnline {
+    using Address for address;
+
+    function safeTransfer(
+        IArtOnline token,
+        address to,
+        uint256 value
+    ) internal {
+        _callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, value));
+    }
+
+    function safeTransferFrom(
+        IArtOnline token,
+        address from,
+        address to,
+        uint256 value
+    ) internal {
+        _callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
+    }
+
+    function safeApprove(
+        IArtOnline token,
+        address spender,
+        uint256 value
+    ) internal {
+        require(
+            (value == 0) || (token.allowance(address(this), spender) == 0),
+            "SafeArtOnline: approve from non-zero to non-zero allowance"
+        );
+        _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, value));
+    }
+
+    function safeIncreaseAllowance(
+        IArtOnline token,
+        address spender,
+        uint256 value
+    ) internal {
+        uint256 newAllowance = token.allowance(address(this), spender) + value;
+        _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
+    }
+
+    function safeDecreaseAllowance(
+        IArtOnline token,
+        address spender,
+        uint256 value
+    ) internal {
+        unchecked {
+            uint256 oldAllowance = token.allowance(address(this), spender);
+            require(oldAllowance >= value, "SafeArtOnline: decreased allowance below zero");
+            uint256 newAllowance = oldAllowance - value;
+            _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
+        }
+    }
+    function _callOptionalReturn(IArtOnline token, bytes memory data) private {
+        bytes memory returndata = address(token).functionCall(data, "SafeArtOnline: low-level call failed");
+        if (returndata.length > 0) {
+            // Return data is optional
+            require(abi.decode(returndata, (bool)), "SafeArtOnline: ERC20 operation did not succeed");
+        }
+    }
+}
