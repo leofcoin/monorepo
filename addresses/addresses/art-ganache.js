f60d574adfa9cffa190c0e7debc626ee5588f3b8
vandeurenglenn <vandeurenglenn@gmail.com>
2021-11-28 16:12:11 +0100

    addresses: add pancakeRoute, staking & wrappedCurrency

---
+++ addresses/addresses/art-ganache.js
@@ -0,0 +1,13 @@
+export default {
+  "access": "0x461FAf739ECE9E16338C25C96Ec9E6d8903aBa90",
+  "bridger": "0x1D2faeF70fb9FEfB492eB90713dE9D7be341809E",
+  "mining": "0x529213436f7E09e2ee9Fb2718a828D47c7560283",
+  "blacklist": "0xb0033d9819BF9AbbE70b1Fd9578982b197E1C8Fd",
+  "artonline": "0xfA885bAE6AD52901561240533a2ceDa2ba35C6f3",
+  "platform": "0x7D662cb5ca410b5df1671D8ED3E8AD88A13766e9",
+  "exchange": "0x82651aD2D162638721A743a07FcB73705Dc4c1dC",
+  "exchangeFactory": "0x70eB4BB1b12D0382c3ec06d169eBa1198FE2bd44",
+  "staking": "0x888ffb9F4F915C054A2A8Ee2fe57e92110DF51c0",
+  "native": "undefined",
+  "panCakeRouter": "undefined"
+}
--- addresses/addresses/binance-smartchain.js
+++ addresses/addresses/binance-smartchain.js
@@ -1,5 +1,7 @@
 export default {
     "artonline": "0x535e67270f4FEb15BFFbFE86FEE308b81799a7a5",
     "platform": "0x1468221dc940909A0a85867d71351d2dA4A3AF0e",
-    "exchange": "0xb764848d11A2924C969Fc1BC8Ed1Ca6d2Db982f0"
+    "exchange": "0xb764848d11A2924C969Fc1BC8Ed1Ca6d2Db982f0",
+    "native": "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
+    "multiCall": "0x41263cba59eb80dc200f3e2544eda4ed6a90e76c"
     }
