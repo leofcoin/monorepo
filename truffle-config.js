c58e6520dc851bf2a9b0f03052b37f22400e2233
vandeurenglenn <vandeurenglenn@gmail.com>
2021-11-28 17:03:00 +0100

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,12 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-// const PrivateKeyProvider = require("web3-privatekey-provider");
-const mnemonic = 'onion view mobile torch box weekend town betray intact slam fabric adjust'
+const dotenv = require('dotenv').config()
+const config = dotenv.parsed
+
 module.exports = {
-  // plugins: ["truffle-contract-size"],
+  plugins: ["truffle-contract-size"],
   compilers: {
     solc: {
-      version: '0.8.0',
+      version: '0.8.7',
       settings: {
         optimizer: {
           enabled: true,
@@ -17,49 +18,26 @@ module.exports = {
     }
   },
   networks: {
-   fork: {
-     host: "127.0.0.1",
-     port: 7545,
-     network_id: "1337"
-   },
-   mainnet: {
-     provider: () => {
-       // const { readFile } = require('fs')
-       // const { promisify } = require('util')
-       // const read = promisify(readFile)
-       // // // const mnemonic = await read('./.mnemonic')
-       // const privateKey = await read('./.privateKey')
-       // return new PrivateKeyProvider('78e88fe3ac5d3fc58b4f6494b75748f944085c8f058878a9a9117df1640cdfb8', 'https://mainnet.infura.io/v3/1ca30fe698514cf19a5e3e5e5c8334a8')
-
-       return new HDWalletProvider({
-         privateKeys: [''],
-         providerOrUrl: 'wss://mainnet.infura.io/ws/v3/1ca30fe698514cf19a5e3e5e5c8334a8'})
-     },
-     network_id: '1'
-   },
-   test: {
-     host: "127.0.0.1",
-     port: 7545,
-     network_id: "*"
-   },
-   ropsten: {
-     provider: () => new HDWalletProvider(mnemonic, 'wss://ropsten.infura.io/ws/v3/1ca30fe698514cf19a5e3e5e5c8334a8'),
-     network_id: '3'
-   },
-   goerli: {
-     provider: () => new HDWalletProvider(mnemonic, 'wss://goerli.infura.io/ws/v3/1ca30fe698514cf19a5e3e5e5c8334a8'),
-     network_id: '5'
-   },
-    kovan: {
-      provider: () => new HDWalletProvider(mnemonic, 'wss://kovan.infura.io/ws/v3/1ca30fe698514cf19a5e3e5e5c8334a8'),
-      network_id: '42'
+    'art-ganache': {
+      provider: () => new HDWalletProvider({
+        privateKeys: [config.TEST_PRIVATEKEY],
+        providerOrUrl: 'HTTP://127.0.0.1:1337'
+      }),
+      network_id: 1337
     },
-   wapnet: {
-     provider: () => new HDWalletProvider(mnemonic, 'http://127.0.0.1:8545'),
-     host: '127.0.0.1',
-     port: 8545,
-     network_id: 7475,
-     gas: 8000000
-   }
+    'binance-smartchain-testnet': {
+      provider: () => new HDWalletProvider({
+        privateKeys: [config.TEST_PRIVATEKEY],
+        providerOrUrl: 'https://data-seed-prebsc-2-s2.binance.org:8545'
+      }),
+      network_id: 97
+    },
+    'binance-smartchain': {
+      provider: () => new HDWalletProvider({
+        privateKeys: [config.MAIN_PRIVATEKEY],
+        providerOrUrl: 'https://bsc-dataseed.binance.org'
+      }),
+      network_id: 56
+    }
   }
 };
