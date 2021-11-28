4a037a9f11d886e136d4128654ec964103ad4ddb
vandeurenglenn <vandeurenglenn@gmail.com>
2021-11-28 16:50:05 +0100

    contracts: add EIP721

---
+++ contracts/token/utils/EIP712.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/utils/cryptography/ECDSA.sol';
+
+contract EIP712 {
+  bytes32 private immutable _CACHED_DOMAIN_SEPARATOR;
+  uint256 private immutable _CACHED_CHAIN_ID;
+
+  bytes32 private immutable _HASHED_NAME;
+  bytes32 private immutable _HASHED_VERSION;
+  bytes32 private immutable _TYPE_HASH;
+
+  constructor(string memory name, string memory version) {
+    bytes32 hashedName = keccak256(bytes(name));
+    bytes32 hashedVersion = keccak256(bytes(version));
+    bytes32 typeHash = keccak256(
+      'EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'
+    );
+    _HASHED_NAME = hashedName;
+    _HASHED_VERSION = hashedVersion;
+    _CACHED_CHAIN_ID = block.chainid;
+    _CACHED_DOMAIN_SEPARATOR = _buildDomainSeparator(typeHash, hashedName, hashedVersion);
+    _TYPE_HASH = typeHash;
+  }
+
+  function _domainSeparatorV4() internal view returns (bytes32) {
+    if (block.chainid == _CACHED_CHAIN_ID) {
+      return _CACHED_DOMAIN_SEPARATOR;
+    } else {
+      return _buildDomainSeparator(_TYPE_HASH, _HASHED_NAME, _HASHED_VERSION);
+    }
+  }
+
+  function _buildDomainSeparator(bytes32 typeHash, bytes32 nameHash, bytes32 versionHash) private view returns (bytes32) {
+    return keccak256(abi.encode(typeHash, nameHash, versionHash, block.chainid, address(this)));
+  }
+
+  function _hashTypedDataV4(bytes32 structHash) internal view virtual returns (bytes32) {
+    return ECDSA.toTypedDataHash(_domainSeparatorV4(), structHash);
+  }
+}
