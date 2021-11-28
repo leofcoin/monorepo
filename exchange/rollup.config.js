7fc50ba356b9fae7a78ebdf46278a9ca800647c1
vandeurenglenn <vandeurenglenn@gmail.com>
2021-11-28 16:47:08 +0100

    exchange: update build config

--- exchange/rollup.config.js
+++ exchange/rollup.config.js
@@ -1,4 +1,4 @@
-// import json from  '@rollup/plugin-json'
+import json from  '@rollup/plugin-json'
 import { execSync } from 'child_process'
 
 execSync('rm -rf www/*.js')
@@ -7,13 +7,13 @@ execSync('cp -rf ./../addresses/addresses/**.js www/addresses')
 
 export default [
 	{
-		input: ['src/shell.js', 'src/views/market.js', 'src/views/home.js', 'src/api/api.js'],
+		input: ['src/shell.js', 'src/views/market.js', 'src/views/list.js', 'src/views/home.js', 'src/api/api.js'],
 		output: {
 			dir: 'www',
 			format: 'es',
 			sourcemap: false
 		},
-		// plugins: [json()],
+		plugins: [json()],
 		external: [
 			'./api.js',
 			'./third-party/ethers.js',
