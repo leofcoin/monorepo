d49d852c3f250d0571e8562d55e06e7bb296e17b
vandeurenglenn <vandeurenglenn@gmail.com>
2021-11-28 15:56:12 +0100

    contracts: Add IWrappedCurrency

---
+++ contracts/exchange/interfaces/IWrappedCurrency.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+interface IWrappedCurrency {
+  function deposit() external;
+  function withdraw() external;
+  function approve(address operator, uint amount) external returns (bool);
+  function transfer(address receiver, uint amount) external returns (bool);
+  function transferFrom(address sender, address receiver, uint amount) external returns (bool);
+}
