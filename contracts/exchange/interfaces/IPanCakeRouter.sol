18ddc72e0fc656f729f071fab20d6bffe02579a2
vandeurenglenn <vandeurenglenn@gmail.com>
2021-12-01 18:43:17 +0100

    contracts: IPanCakeRouter add swapExactTokensForTokens

--- contracts/exchange/interfaces/IPanCakeRouter.sol
+++ contracts/exchange/interfaces/IPanCakeRouter.sol
@@ -4,4 +4,5 @@ pragma solidity 0.8.7;
 
 interface IPanCakeRouter {
    function getAmountsOut(uint amountIn, address[] memory path) external view returns (uint[] memory amounts);
+   function swapExactTokensForTokens(uint256, uint256, address[] memory, address, uint256) external;
 }
