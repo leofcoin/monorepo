b63e40a9c3cd74d6cc798446dfd372ad97961d74
vandeurenglenn <vandeurenglenn@gmail.com>
2021-12-01 18:44:03 +0100

    contracts: IWrappedCurrency fix deposit not-payable

--- contracts/exchange/interfaces/IWrappedCurrency.sol
+++ contracts/exchange/interfaces/IWrappedCurrency.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.7;
 
 interface IWrappedCurrency {
-  function deposit() external;
+  function deposit() external payable;
   function withdraw() external;
   function approve(address operator, uint amount) external returns (bool);
   function transfer(address receiver, uint amount) external returns (bool);
