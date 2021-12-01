717697abc30640af134104cc64363e2f6fe5a731
vandeurenglenn <vandeurenglenn@gmail.com>
2021-12-01 18:42:12 +0100

    contracts: IArtOnlineListing partner -> splitter

--- contracts/exchange/interfaces/IArtOnlineListing.sol
+++ contracts/exchange/interfaces/IArtOnlineListing.sol
@@ -7,7 +7,7 @@ interface IArtOnlineListing {
   function tokenId() external returns (uint256);
   function owner() external returns (address);
   function setOwner(address owner) external;
-  function buy(address receiver) external;
+  function buy(address receiver) external payable;
   function setPrice(uint256 price) external;
   function price() external returns (uint256);
   function delist() external;
@@ -16,5 +16,5 @@ interface IArtOnlineListing {
   function listed() external returns (uint256);
   function currency() external view returns (address);
   function setCurrency(address currency_, uint256 price_) external;
-  function setPartner(address partner) external;
+  function setSplitter(address splitter) external;
 }
