76e16202f88c3de4d4f89f67f6894a414eae1126
vandeurenglenn <vandeurenglenn@gmail.com>
2021-12-09 20:54:08 +0100

    Update IArtOnlineStaking.sol

--- contracts/staking/interfaces/IArtOnlineStaking.sol
+++ contracts/staking/interfaces/IArtOnlineStaking.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.0;
 
 interface IArtOnlineStaking {
   function totalSupply() external view returns (uint256);
-  function balanceOf(address account) external view returns (uint256);
+  function balanceOf(address, address account) external view returns (uint256);
   function setReleaseTime(uint256 releaseTime_) external;
   function releaseTime() external view returns (uint256);
-  function stake(address account, uint256 amount) external returns (bytes32);
+  function stake(address account, uint256 amount, address) external returns (bytes32);
   function withdraw(address account, bytes32 id) external;
-  function holders() external returns (address[] memory);
+  function holders(address) external returns (address[] memory);
   function readyToRelease(address account, bytes32 id) external view returns (bool);
 }
