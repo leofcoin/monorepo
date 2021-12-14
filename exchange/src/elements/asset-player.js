0359cbf3a556badd7e588a0316a2f2d60ea60762
vandeurenglenn <vandeurenglenn@gmail.com>
2021-12-14 21:07:13 +0100

    exchange: add asset-player

---
+++ exchange/src/elements/asset-player.js
@@ -0,0 +1,65 @@
+export default customElements.define('asset-player', class AssetPlayer extends BaseClass {
+  static get observedAttributes() {
+    return ['src']
+  }
+  constructor() {
+    super()
+  }
+
+  attributeChangedCallback(name, old, value) {
+    if (value && old !== value) this[name] = value
+  }
+
+  get _video() {
+    return this.shadowRoot.querySelector('video')
+  }
+
+  get _img() {
+    return this.shadowRoot.querySelector('img')
+  }
+
+  set src(value) {
+    console.log(value.includes('.mp4'));
+    if (value.includes('.mp4')) {
+      this._img.setAttribute('hidden', '')
+      console.log(this._img);
+      const source = document.createElement('source')
+      source.src = value
+      this._video.appendChild(source)
+      this._video.removeAttribute('hidden')
+    } else {
+      console.log("else");
+      this._video.setAttribute('hidden', '')
+      this._img.src = value
+      this._img.removeAttribute('hidden')
+    }
+  }
+
+  get template() {
+    return html`
+    <style>
+      :host {
+        display: flex;
+        height: 100%;
+        width: 100%;
+        border-radius: 12px;
+        max-height: 150px;
+        overflow: hidden;
+        background: #000;
+        ${miniframe.styles.elevation.elevation6dp}
+      }
+      img, video {
+        height: 100%;
+        width: 100%;
+        opacity: 1;
+      }
+
+      [hidden] {
+        opacity: 0;
+      }
+    </style>
+    <img></img>
+    <video autoplay="true" loop="true" muted="true"></video>
+    `
+  }
+})
