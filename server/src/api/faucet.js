fc7e6e3bc63f2d4df19281a28e2bc4f7e8a08910
vandeurenglenn <vandeurenglenn@gmail.com>
2022-02-09 21:19:22 +0100

    server: add faucet api

---
+++ server/src/api/faucet.js
@@ -0,0 +1,61 @@
+// import cache from './../cache'
+import ABI from './../../../abis/artonline'
+import mime from 'mime-types'
+import ethers from 'ethers'
+import Router from '@koa/router'
+const router = new Router()
+
+const timedOut = {}
+
+
+const dotenv = require('dotenv').config()
+const config = dotenv.parsed
+
+const network = 'binance-smartchain-testnet'
+
+const rpcUrls =  {
+  'art-ganache': 'http://127.0.0.1:1337',
+  'binance-smartchain-testnet': 'https://data-seed-prebsc-1-s1.binance.org:8545',
+  'binance-smartchain': 'https://bsc-dataseed.binance.org'
+}
+const chainIds =  {
+  'art-ganache': 1337,
+  'binance-smartchain-testnet': 97,
+  'binance-smartchain': 56
+}
+
+let addresses = require(join(__dirname, `./../../../addresses/addresses/${network}.json`))
+
+const provider = new ethers.providers.JsonRpcProvider( rpcUrls[network], {
+  chainId: chainIds[network]
+})
+// 127.0.0.1:1337
+
+const signer = new ethers.Wallet(config.FAUCET_PRIVATEKEY, provider)
+
+const contract = new ethers.Contract(addresses.artonline, ABI, signer)
+
+const timedOutMessage = ctx => {
+  ctx.body = `${ctx.request.query.address} on timeout`
+}
+
+router.get('/faucet', ctx => {
+  if (timedOut[ctx.request.header['cf-connecting-ip']]) return timedOutMessage(ctx)
+  if (timedOut[ctx.resuest.query.address]) return timedOutMessage(ctx)
+  const time = new Date().getTime() + 8.64e+7
+  timedOut[ctx.resuest.query.address] = time
+  timedOut[ctx.request.header['cf-connecting-ip']] = time
+  let tx = await contract.transfer(ctx.resuest.query.address, ethers.utils.parseUnits('50000'))
+  // console.log(tx);
+  ctx.body = tx.hash
+})
+
+router.get('/faucet/tot', ctx => {
+  if (timedOut[ctx.request.header['cf-connecting-ip']])
+    String(timedOut[ctx.request.header['cf-connecting-ip']])
+
+  if (ctx.request.query.address) ctx.body =
+    String(timedOut[ctx.resuest.query.address])
+})
+
+export default router
