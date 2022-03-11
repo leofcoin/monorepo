6bc2a48afd810f74009fb3730ee32795a7b3709a
vandeurenglenn <vandeurenglenn@gmail.com>
2022-03-11 18:45:44 +0100

    lottery: update countdown, show seconds & drawing

--- lottery/src/elements/countdown.js
+++ lottery/src/elements/countdown.js
@@ -6,17 +6,28 @@ export default customElements.define('custom-countdown', class CustomCountdown e
   }
 
   set value(value) {
+    this.value_ = value
     const currentTime = Math.round(new Date().getTime() / 1000)
     value = value - currentTime
     const hours = Math.floor((value % (60 * 60 * 24)) / (60 * 60));
     const minutes = Math.floor((value % (60 * 60)) / (60));
+    const seconds = Math.floor((value % 60));
 
     if (hours !== 0) {
       value = `${hours} ${hours > 1 ? 'hours' : 'hour'} ${minutes} ${minutes > 1 ? 'minutes' : 'minute'}`
+    } else if (minutes !== 0) {
+      value = `${minutes} ${minutes > 1 ? 'minutes' : 'minute'} ${seconds} ${seconds > 1 ? 'seconds': 'second'}`
     } else {
-      value = `${minutes} ${minutes > 1 ? 'minutes' : 'minute'}`
+      value = `${seconds} ${seconds > 1 ? 'seconds': 'second'}`
+    }
+    if (hours === -1 && minutes === -1) {
+      value = 'Drawing'
     }
     this.innerHTML = value
+  }
+
+  get value() {
+    return this.value_
   }
 
   get template() {
