179a6eaaa0dbf72673c26fce9874b21f85e59b0e
vandeurenglenn <vandeurenglenn@gmail.com>
2022-03-27 21:07:29 +0200

    lottery-master: cleanup

--- lottery-master/src/service.js
+++ lottery-master/src/service.js
@@ -12,9 +12,6 @@ const run = async () => {
   const ticketContract = new ethers.Contract(lotteryMaster.addresses.lotteryTickets, TICKETS_ABI, lotteryMaster.signer)
   let currentLottery = await contract.latestLottery()
   const currentTime = Math.round(new Date().getTime() / 1000)
-  // const currentTime = await contract.timestamp()
-  console.log(currentTime);
-  console.log(currentLottery.endTime.toNumber(), currentLottery.endTime.toNumber() < currentTime);
   if ((currentLottery.endTime.toNumber() * 1000) > new Date().getTime()) {
 
     _timeout = Math.round((currentLottery.endTime.toNumber() * 1000) - new Date().getTime()) - 3000
@@ -24,38 +21,36 @@ const run = async () => {
     const numbers = lottery(6, 9)
     let supply = await ticketContract.totalSupply(currentLottery.id)
     let promises = []
-    for (let i = 0; i <= supply; i++) {
+    for (let i = 1; i <= supply; i++) {
       promises.push(ticketContract.getTicketNumbers(currentLottery.id, i))
     }
     promises = await Promise.all(promises)
     promises = promises.reduce((p, c, i) => {
       let matches = 0
-      c = c.map(number => number.toNumber())
-      c = c.join('')
-      if (c.charAt(0) === numbers.join('').charAt(0)) matches += 1
-      if (c.charAt(1) === numbers.join('').charAt(1)) matches += 1
-      if (c.charAt(2) === numbers.join('').charAt(2)) matches += 1
-      if (c.charAt(3) === numbers.join('').charAt(3)) matches += 1
-      if (c.charAt(4) === numbers.join('').charAt(4)) matches += 1
-      if (c.charAt(5) === numbers.join('').charAt(5)) matches += 1
+      if (c.length !== 0) {
+        c = c.join('')
+        if (c.charAt(0) === numbers.join('').charAt(0)) matches += 1
+        if (c.charAt(1) === numbers.join('').charAt(1)) matches += 1
+        if (c.charAt(2) === numbers.join('').charAt(2)) matches += 1
+        if (c.charAt(3) === numbers.join('').charAt(3)) matches += 1
+        if (c.charAt(4) === numbers.join('').charAt(4)) matches += 1
+        if (c.charAt(5) === numbers.join('').charAt(5)) matches += 1
 
-      if (matches > 0) p.push({ticket: i, matches})
+        if (matches > 0) p.push({ticket: i, matches})
+      }
+
       return p
     }, [])
-    console.log(promises);
     promises = promises.reduce((set, c) => {
-      if (!set[Number(c.matches)]) set[c.matches] = 0;
-
-      set[Number(c.matches)] += 1
-
+      set[Number(c.matches - 1)] += 1
       return set
-    }, [])
+    }, [0,0,0,0,0,0])
 
     console.log(promises);
     try {
       const ticketPrice = ethers.utils.parseUnits('1000', 18)
       const startDelay = 10
-      const duration = 240
+      const duration = 3600
       const pot = 100000
       tx = await lotteryMaster.create(
         ticketPrice,
