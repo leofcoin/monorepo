e505b0e74faed177acab3ae748d89c4a6770a179
vandeurenglenn <vandeurenglenn@gmail.com>
2022-03-28 17:25:01 +0200

    project-deploy: add module example

--- project-deploy/deploy.js
+++ project-deploy/deploy.js
@@ -1,3 +1,8 @@
-export default (deployer) => {
-  await deployer.deploy('contracts/TestContract', ['0x4eCfe05bAe2535f13a92A16E60Be1b68BdEDEDb7'])
-}
+import deploy from './src/project-deploy.js'
+
+const network = 'binance-smartchain-testnet';
+
+(async (deployer) => {
+  const result = await deploy('contracts/TestContract.sol', ['0x4eCfe05bAe2535f13a92A16E60Be1b68BdEDEDb7'], network)
+  console.log(result);
+})()
