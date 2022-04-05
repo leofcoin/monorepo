ff547bb76c25476d2e9319c76537b0b4a79b17c5
vandeurenglenn <vandeurenglenn@gmail.com>
2022-04-05 19:55:42 +0200

    chain: add block message

---
+++ chain/src/messages/block.js
@@ -0,0 +1,14 @@
+import protons from 'protons'
+import proto from './../protos/block.proto.js'
+import CodecFormat from './../../node_modules/@leofcoin/peernet/src/codec/codec-format-interface'
+
+export default class BlockMessage extends CodecFormat {
+  get keys() {
+    return ['index', 'previousHash', 'timestamp', 'reward', 'fees', 'transactions', 'validators']
+  }
+
+  constructor(buffer) {
+    const name = 'block-message'
+    super(buffer, protons(proto).BlockMessage, {name})
+  }
+}
