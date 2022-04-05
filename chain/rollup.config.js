36578736c6433a41c469b35bb3ae8a3cca4d6280
vandeurenglenn <vandeurenglenn@gmail.com>
2022-04-05 19:51:28 +0200

    chain: update build config

--- chain/rollup.config.js
+++ chain/rollup.config.js
@@ -1,24 +1,122 @@
 import cjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import json from '@rollup/plugin-json'
+import strip from '@rollup/plugin-strip';
+import { terser } from "rollup-plugin-terser";
+import modify from 'rollup-plugin-modify'
+
 export default [{
-  input: ['./src/chain.js', 'src/interfaces/token.js'],
+  input: ['./src/chain.js'],
+  output: {
+    dir: './dist',
+    format: 'cjs'
+  },
+  plugins: [
+    json()
+    // terser({
+    //   keep_classnames: true
+    // }),
+    // cjs()
+  ]
+}, {
+  input: ['./src/lib.js'],
+  output: {
+    file: 'lib.js',
+    format: 'cjs'
+  },
+  plugins: [
+    json(),
+    terser({
+      keep_classnames: true
+    }),
+    // cjs()
+  ]
+}, {
+  input: ['./src/node.js'],
   output: {
     dir: 'dist',
     format: 'cjs'
   },
   plugins: [
     json(),
-    // resolve({preferBuiltins: false,browser: false, extensions: ['.js', '.mjs', '.node']}),
+    terser({
+      keep_classnames: true
+    }),
+    // cjs()
   ]
 }, {
-  input: ['src/token/native.js'],
+  input: ['src/contracts/factory.js'],
   output: {
-    dir: 'dist',
+    dir: 'dist/contracts',
     format: 'es'
   },
   plugins: [
     json(),
+    strip(),
+    terser({
+      mangle: true,
+      keep_classnames: true
+    })
+    // resolve({preferBuiltins: false,browser: false, extensions: ['.js', '.mjs', '.node']}),
+  ]
+}, {
+  input: ['src/contracts/nameService.js'],
+  output: {
+    dir: 'dist/contracts',
+    format: 'es'
+  },
+  plugins: [
+    json(),
+    strip(),
+    terser({
+      mangle: true,
+      keep_classnames: true
+    })
+    // resolve({preferBuiltins: false,browser: false, extensions: ['.js', '.mjs', '.node']}),
+  ]
+}, {
+  input: ['src/contracts/nativeToken.js'],
+  output: {
+    dir: 'dist/contracts',
+    format: 'es'
+  },
+  plugins: [
+    json(),
+    strip(),
+    terser({
+      mangle: true,
+      keep_classnames: true
+    })
+    // resolve({preferBuiltins: false,browser: false, extensions: ['.js', '.mjs', '.node']}),
+  ]
+},{
+  input: ['src/contracts/validators.js'],
+  output: {
+    dir: 'dist/contracts',
+    format: 'es'
+  },
+  plugins: [
+    json(),
+    strip(),
+    terser({
+      mangle: true,
+      keep_classnames: true
+    })
+    // resolve({preferBuiltins: false,browser: false, extensions: ['.js', '.mjs', '.node']}),
+  ]
+}, {
+  input: ['src/standards/token.js'],
+  output: {
+    dir: 'dist/standards',
+    format: 'es'
+  },
+  plugins: [
+    json(),
+    strip(),
+    terser({
+      mangle: true,
+      keep_classnames: true
+    })
     // resolve({preferBuiltins: false,browser: false, extensions: ['.js', '.mjs', '.node']}),
   ]
 }]
