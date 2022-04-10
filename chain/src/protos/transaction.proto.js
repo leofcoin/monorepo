354ab5d024e0b5a5b44b505009cdfe2790116a42
vandeurenglenn <vandeurenglenn@gmail.com>
2022-04-10 22:09:04 +0200

    chain: update transaction to latest standard

--- chain/src/protos/transaction.proto.js
+++ chain/src/protos/transaction.proto.js
@@ -4,7 +4,8 @@ message TransactionMessage {
   required uint64 timestamp = 1;
   required string from = 2;
   required string to = 3;
-  required string method = 4;
-  repeated string params = 5;
+  required uint64 nonce = 4;
+  required string method = 5;
+  repeated string params = 6;
 }
 `
