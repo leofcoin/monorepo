ed59e70694dda666fecebae786f669d8583b4c45
vandeurenglenn <vandeurenglenn@gmail.com>
2022-04-16 19:04:49 +0200

    swarm: add server utils

---
+++ peernet-swarm/src/server/utils.js
@@ -0,0 +1,21 @@
+import socketResponse from './../../node_modules/socket-request-server/src/socket-response'
+
+/**
+ * @params {String} event - name of the pubsub.subscribtion (peer:left||peer:joined)
+ * @params {String} data - peer id
+ * @params {String} connection - peer connection
+ */
+export const sendPubSubMessage = (event, data, connection) => {
+  socketResponse(connection, 'pubsub', event).send(data)
+}
+
+/**
+ * @params {String} event - name of the pubsub.subscribtion (peer:left||peer:joined)
+ * @params {String} data - peer id
+ * @params {Object} peers - peerset to broadcast todo
+ */
+export const broadcastPubSubMessage = (event, data, peers) => {
+  for (const connection of Object.values(peers)) {
+    socketResponse(connection, 'pubsub', event).send(data)
+  }
+}
