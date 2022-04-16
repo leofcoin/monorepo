8734dfef3e25bd309d88f128e9d09d466faa9170
vandeurenglenn <vandeurenglenn@gmail.com>
2022-04-16 19:10:01 +0200

    assets: add simple manifest

--- assets/manifest.js
+++ assets/manifest.js
@@ -1,6 +1,332 @@
-export default [
-  {
-    name: '',
-    src: ''
-  }
-]
+export default {
+	"adam": {
+		"path": "adam.png",
+		"name": "adam",
+		"extension": ".png",
+		"url": "https://assets.artonline.site//adam.png"
+	},
+	"arteon-dark": {
+		"path": "arteon-dark.png",
+		"name": "arteon-dark",
+		"extension": ".png",
+		"url": "https://assets.artonline.site//arteon-dark.png"
+	},
+	"arteon-old": {
+		"path": "arteon-old.svg",
+		"name": "arteon-old",
+		"extension": ".svg",
+		"url": "https://assets.artonline.site//arteon-old.svg"
+	},
+	"arteon": {
+		"path": "arteon.svg",
+		"name": "arteon",
+		"extension": ".svg",
+		"url": "https://assets.artonline.site//arteon.svg"
+	},
+	"bhero": {
+		"path": "bhero.png",
+		"name": "bhero",
+		"extension": ".png",
+		"url": "https://assets.artonline.site//bhero.png"
+	},
+	"exchange": {
+		"path": "exchange.png",
+		"name": "exchange",
+		"extension": ".png",
+		"url": "https://assets.artonline.site//exchange.png"
+	},
+	"faucet": {
+		"path": "faucet.png",
+		"name": "faucet",
+		"extension": ".png",
+		"url": "https://assets.artonline.site//faucet.png"
+	},
+	"favicon-16x16": {
+		"path": "favicon-16x16.png",
+		"name": "favicon-16x16",
+		"extension": ".png",
+		"url": "https://assets.artonline.site//favicon-16x16.png"
+	},
+	"favicon-32x32": {
+		"path": "favicon-32x32.png",
+		"name": "favicon-32x32",
+		"extension": ".png",
+		"url": "https://assets.artonline.site//favicon-32x32.png"
+	},
+	"full_spec": {
+		"path": "full_spec.png",
+		"name": "full_spec",
+		"extension": ".png",
+		"url": "https://assets.artonline.site//full_spec.png"
+	},
+	"glenn": {
+		"path": "glenn.png",
+		"name": "glenn",
+		"extension": ".png",
+		"url": "https://assets.artonline.site//glenn.png"
+	},
+	"icon-192x192": {
+		"path": "icon-192x192.png",
+		"name": "icon-192x192",
+		"extension": ".png",
+		"url": "https://assets.artonline.site//icon-192x192.png"
+	},
+	"icon-256x256": {
+		"path": "icon-256x256.png",
+		"name": "icon-256x256",
+		"extension": ".png",
+		"url": "https://assets.artonline.site//icon-256x256.png"
+	},
+	"icon-384x384": {
+		"path": "icon-384x384.png",
+		"name": "icon-384x384",
+		"extension": ".png",
+		"url": "https://assets.artonline.site//icon-384x384.png"
+	},
+	"icon-512x512": {
+		"path": "icon-512x512.png",
+		"name": "icon-512x512",
+		"extension": ".png",
+		"url": "https://assets.artonline.site//icon-512x512.png"
+	},
+	"jack": {
+		"path": "jack.png",
+		"name": "jack",
+		"extension": ".png",
+		"url": "https://assets.artonline.site//jack.png"
+	},
+	"john": {
+		"path": "john.png",
+		"name": "john",
+		"extension": ".png",
+		"url": "https://assets.artonline.site//john.png"
+	},
+	"kyle": {
+		"path": "kyle.png",
+		"name": "kyle",
+		"extension": ".png",
+		"url": "https://assets.artonline.site//kyle.png"
+	},
+	"logo-200x200": {
+		"path": "logo-200x200.png",
+		"name": "logo-200x200",
+		"extension": ".png",
+		"url": "https://assets.artonline.site//logo-200x200.png"
+	},
+	"lottery": {
+		"path": "lottery.png",
+		"name": "lottery",
+		"extension": ".png",
+		"url": "https://assets.artonline.site//lottery.png"
+	},
+	"metamask-fox": {
+		"path": "metamask-fox.svg",
+		"name": "metamask-fox",
+		"extension": ".svg",
+		"url": "https://assets.artonline.site//metamask-fox.svg"
+	},
+	"mike": {
+		"path": "mike.png",
+		"name": "mike",
+		"extension": ".png",
+		"url": "https://assets.artonline.site//mike.png"
+	},
+	"platform": {
+		"path": "platform.png",
+		"name": "platform",
+		"extension": ".png",
+		"url": "https://assets.artonline.site//platform.png"
+	},
+	"tears": {
+		"path": "tears.png",
+		"name": "tears",
+		"extension": ".png",
+		"url": "https://assets.artonline.site//tears.png"
+	},
+	"yorick": {
+		"path": "yorick.png",
+		"name": "yorick",
+		"extension": ".png",
+		"url": "https://assets.artonline.site//yorick.png"
+	},
+	"ARTX 1000-320": {
+		"path": "cards/ARTX 1000-320.png",
+		"name": "ARTX 1000-320",
+		"extension": ".png",
+		"url": "https://assets.artonline.site/cards/ARTX 1000-320.png"
+	},
+	"ARTX 1000-720": {
+		"path": "cards/ARTX 1000-720.png",
+		"name": "ARTX 1000-720",
+		"extension": ".png",
+		"url": "https://assets.artonline.site/cards/ARTX 1000-720.png"
+	},
+	"ARTX 2000-320": {
+		"path": "cards/ARTX 2000-320.png",
+		"name": "ARTX 2000-320",
+		"extension": ".png",
+		"url": "https://assets.artonline.site/cards/ARTX 2000-320.png"
+	},
+	"ARTX 2000-720": {
+		"path": "cards/ARTX 2000-720.png",
+		"name": "ARTX 2000-720",
+		"extension": ".png",
+		"url": "https://assets.artonline.site/cards/ARTX 2000-720.png"
+	},
+	"GENESIS-320": {
+		"path": "cards/GENESIS-320.png",
+		"name": "GENESIS-320",
+		"extension": ".png",
+		"url": "https://assets.artonline.site/cards/GENESIS-320.png"
+	},
+	"GENESIS-720": {
+		"path": "cards/GENESIS-720.png",
+		"name": "GENESIS-720",
+		"extension": ".png",
+		"url": "https://assets.artonline.site/cards/GENESIS-720.png"
+	},
+	"GOLDGEN-320": {
+		"path": "cards/GOLDGEN-320.png",
+		"name": "GOLDGEN-320",
+		"extension": ".png",
+		"url": "https://assets.artonline.site/cards/GOLDGEN-320.png"
+	},
+	"MODULE-320": {
+		"path": "cards/MODULE-320.png",
+		"name": "MODULE-320",
+		"extension": ".png",
+		"url": "https://assets.artonline.site/cards/MODULE-320.png"
+	},
+	"SHIBOKI-320": {
+		"path": "cards/SHIBOKI-320.png",
+		"name": "SHIBOKI-320",
+		"extension": ".png",
+		"url": "https://assets.artonline.site/cards/SHIBOKI-320.png"
+	},
+	"XTREME-320": {
+		"path": "cards/XTREME-320.png",
+		"name": "XTREME-320",
+		"extension": ".png",
+		"url": "https://assets.artonline.site/cards/XTREME-320.png"
+	},
+	"_ARTX 1000-320": {
+		"path": "cards/_ARTX 1000-320.png",
+		"name": "_ARTX 1000-320",
+		"extension": ".png",
+		"url": "https://assets.artonline.site/cards/_ARTX 1000-320.png"
+	},
+	"_ARTX 2000-320": {
+		"path": "cards/_ARTX 2000-320.png",
+		"name": "_ARTX 2000-320",
+		"extension": ".png",
+		"url": "https://assets.artonline.site/cards/_ARTX 2000-320.png"
+	},
+	"_GENESIS-320": {
+		"path": "cards/_GENESIS-320.png",
+		"name": "_GENESIS-320",
+		"extension": ".png",
+		"url": "https://assets.artonline.site/cards/_GENESIS-320.png"
+	},
+	"ARTX 1000": {
+		"path": "fans/ARTX 1000.png",
+		"name": "ARTX 1000",
+		"extension": ".png",
+		"url": "https://assets.artonline.site/fans/ARTX 1000.png"
+	},
+	"ARTX 2000": {
+		"path": "fans/ARTX 2000.png",
+		"name": "ARTX 2000",
+		"extension": ".png",
+		"url": "https://assets.artonline.site/fans/ARTX 2000.png"
+	},
+	"GENESIS": {
+		"path": "fans/GENESIS.png",
+		"name": "GENESIS",
+		"extension": ".png",
+		"url": "https://assets.artonline.site/fans/GENESIS.png"
+	},
+	"GOLDGEN": {
+		"path": "fans/GOLDGEN.png",
+		"name": "GOLDGEN",
+		"extension": ".png",
+		"url": "https://assets.artonline.site/fans/GOLDGEN.png"
+	},
+	"MODULE": {
+		"path": "fans/MODULE.png",
+		"name": "MODULE",
+		"extension": ".png",
+		"url": "https://assets.artonline.site/fans/MODULE.png"
+	},
+	"SHIBOKI-1": {
+		"path": "fans/SHIBOKI-1.png",
+		"name": "SHIBOKI-1",
+		"extension": ".png",
+		"url": "https://assets.artonline.site/fans/SHIBOKI-1.png"
+	},
+	"SHIBOKI": {
+		"path": "fronts/SHIBOKI.png",
+		"name": "SHIBOKI",
+		"extension": ".png",
+		"url": "https://assets.artonline.site/fronts/SHIBOKI.png"
+	},
+	"SPINNER": {
+		"path": "fronts/SPINNER.png",
+		"name": "SPINNER",
+		"extension": ".png",
+		"url": "https://assets.artonline.site/fronts/SPINNER.png"
+	},
+	"XTREME-1": {
+		"path": "fans/XTREME-1.png",
+		"name": "XTREME-1",
+		"extension": ".png",
+		"url": "https://assets.artonline.site/fans/XTREME-1.png"
+	},
+	"XTREME": {
+		"path": "fronts/XTREME.png",
+		"name": "XTREME",
+		"extension": ".png",
+		"url": "https://assets.artonline.site/fronts/XTREME.png"
+	},
+	"discord-black": {
+		"path": "social/discord-black.svg",
+		"name": "discord-black",
+		"extension": ".svg",
+		"url": "https://assets.artonline.site/social/discord-black.svg"
+	},
+	"discord-color": {
+		"path": "social/discord-color.svg",
+		"name": "discord-color",
+		"extension": ".svg",
+		"url": "https://assets.artonline.site/social/discord-color.svg"
+	},
+	"discord-white": {
+		"path": "social/discord-white.svg",
+		"name": "discord-white",
+		"extension": ".svg",
+		"url": "https://assets.artonline.site/social/discord-white.svg"
+	},
+	"telegram-white": {
+		"path": "social/telegram-white.svg",
+		"name": "telegram-white",
+		"extension": ".svg",
+		"url": "https://assets.artonline.site/social/telegram-white.svg"
+	},
+	"twitter-black": {
+		"path": "social/twitter-black.svg",
+		"name": "twitter-black",
+		"extension": ".svg",
+		"url": "https://assets.artonline.site/social/twitter-black.svg"
+	},
+	"twitter-color": {
+		"path": "social/twitter-color.svg",
+		"name": "twitter-color",
+		"extension": ".svg",
+		"url": "https://assets.artonline.site/social/twitter-color.svg"
+	},
+	"twitter-white": {
+		"path": "social/twitter-white.svg",
+		"name": "twitter-white",
+		"extension": ".svg",
+		"url": "https://assets.artonline.site/social/twitter-white.svg"
+	}
+}
