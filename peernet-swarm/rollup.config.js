114f302a0bbf5cecbff27b6aa7b977830445da7d
vandeurenglenn <vandeurenglenn@gmail.com>
2022-04-17 15:09:17 +0200

    swarm: update build config

--- peernet-swarm/rollup.config.js
+++ peernet-swarm/rollup.config.js
@@ -1,8 +1,19 @@
+import {execSync} from 'child_process'
+
+try {
+  execSync('rm ./dist/*.js')
+} catch (e) {
+  console.log('nothing to clean');
+}
+
 export default  [{
   input: ['src/index.js', 'src/server/server.js', 'src/client/client.js'],
-  output: {
-    dir: './dist',
+  output: [{
+    dir: './dist/es',
+    format: 'es'
+  }, {
+    dir: './dist/commonjs',
     format: 'cjs',
     exports: 'auto'
-  }
+  }]
 }]
