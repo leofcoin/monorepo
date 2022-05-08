439ca4e3556291b804ef0ef439f1f9636657f946
vandeurenglenn <vandeurenglenn@gmail.com>
2022-05-08 21:59:05 +0200

    peernet-swarm: Fix big messages & introduce experimental sharding

--- peernet-swarm/src/client/peer.js
+++ peernet-swarm/src/client/peer.js
@@ -1,5 +1,7 @@
 import '@vandeurenglenn/debug'
 
+const messageQue = {}
+
 export default class Peer {
   #connection
   #ready = false
@@ -18,6 +20,7 @@ export default class Peer {
   #iceCompleteTimer
   #channel
   #peerId
+  #chunkSize = 16384
 
   get connection() {
     return this.#connection
@@ -69,29 +72,51 @@ export default class Peer {
      return this._socketClient
    }
 
-   send(message) {
-     switch (this.channel?.readyState) {
-       case 'open':
-        this.bw.up += message.length || message.byteLength
-        this.channel.send(message)
-       break;
-       case 'closed':
-       case 'closing':
-        debug('channel already closed, this usually means a bad implementation, try checking the readyState or check if the peer is connected before sending')
-       break;
-       case undefined:
-       debug(`trying to send before a channel is created`)
-       break;
-     }
+   splitMessage(message) {
+     const chunks = []
+     const size = message.byteLength || message.length
+     let offset = 0
+     return new Promise((resolve, reject) => {
+       const splitMessage = () => {
+         const chunk = message.slice(offset, offset + this.#chunkSize > size ? size : offset + this.#chunkSize)
+         offset += offset + this.#chunkSize
+         chunks.push(chunk)
+         if (offset < size) return splitMessage()
+         else resolve({chunks, size})
+       }
+
+       splitMessage()
+     })
+   }
+
+   async send(message, id) {
+     const { chunks, size } = await this.splitMessage(data)
+     let offset = 0
+     for (const chunk of chunks) {
+        const start = offset
+        const end = offset + chunk.length
+        const message = new TextEncoder().encode(JSON.stringify({ size, id, chunk, start, end }));
+        switch (this.channel?.readyState) {
+          case 'open':
+           this.bw.up += message.length || message.byteLength;
+           this.channel.send(message);
+          break;
+          case 'closed':
+          case 'closing':
+           debug('channel already closed, this usually means a bad implementation, try checking the readyState or check if the peer is connected before sending');
+          break;
+          case undefined:
+          debug(`trying to send before a channel is created`);
+          break;
+        }
+      }
    }
 
    request(data) {
     return new Promise((resolve, reject) => {
       const id = Math.random().toString(36).slice(-12)
-      // TODO: get rid of JSON
-      data = new TextEncoder().encode(JSON.stringify({id, data}))
+
       const _onData = message => {
-        message = JSON.parse(new TextDecoder().decode(message.data))
         if (message.id === id) {
           resolve(message.data)
           pubsub.unsubscribe(`peer:data`, _onData)
@@ -105,8 +130,8 @@ export default class Peer {
       //   pubsub.unsubscribe(`peer:data-request-${id}`, _onData)
       // }, 5000);
 
-      this.send(data)
-    });
+      this.send(data, id)
+    })
   }
 
    async #init() {
@@ -125,7 +150,7 @@ export default class Peer {
            this.ipFamily = this.address.includes('::') ? 'ipv6': 'ipv4'
            this._sendMessage({candidate})
          }
-       };
+       }
        // if (this.initiator) this.#connection.onnegotiationneeded = () => {
          // console.log('create offer');
        this.#connection.ondatachannel = (message) => {
@@ -136,9 +161,7 @@ export default class Peer {
          message.channel.onclose = () => this.close.bind(this)
 
          message.channel.onmessage = (message) => {
-           pubsub.publish('peer:data', message)
-           debug(`incoming message from ${this.peerId}`)
-           this.bw.down += message.length || message.byteLength
+           this._handleMessage(this.id, message)
          }
          this.channel = message.channel
        }
@@ -153,19 +176,39 @@ export default class Peer {
         this.channel.onclose = () => this.close.bind(this)
 
         this.channel.onmessage = (message) => {
-          pubsub.publish('peer:data', message)
-          debug(`incoming message from ${this.peerId}`)
-          this.bw.down += message.length || message.byteLength
+          this._handleMessage(this.peerId, message)
         }
 
        const offer = await this.#connection.createOffer()
        await this.#connection.setLocalDescription(offer)
 
        this._sendMessage({'sdp': this.#connection.localDescription})
-
+     }
      } catch (e) {
        console.log(e);
      }
+   }
+
+   _handleMessage(peerId, message) {
+     debug(`incoming message from ${peerId}`)
+
+     message = JSON.parse(new TextDecoder().decode(message.data))
+     // allow sharding (multiple peers share data)
+     pubsub.publish('peernet:shard', message)
+     if (!messageQue[message.id]) messageQue[message.id] = []
+
+     if (message.size > messageQue[message.id].length || message.size === messageQue[message.id].length) {
+       for (const value of Object.values(message.chunk)) {
+         messageQue[message.id].push(value)
+       }
+     }
+
+     if (message.size === messageQue[message.id].length) {
+       pubsub.publish('peer:data', {id: message.id, data: new Uint8Array(Object.values(messageQue[message.id]))})
+       delete messageQue[message.id]
+     }
+     this.bw.down += message.byteLength || message.length
+
    }
 
    _sendMessage(message) {
