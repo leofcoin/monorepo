90876c6f3f6b768d6f7165db153646d919e792a7
vandeurenglenn <vandeurenglenn@gmail.com>
2022-05-15 18:22:30 +0200

    chain: build with webpack

---
+++ chain/webpack.config.js
@@ -0,0 +1,85 @@
+const path = require('path');
+const webpack = require('webpack');
+module.exports = [{
+  entry: './src/chain.js',
+  plugins: [
+    // Work around for Buffer is undefined:
+    // https://github.com/webpack/changelog-v5/issues/10
+    new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+    }),
+    new webpack.ProvidePlugin({
+        process: 'process/browser',
+  })
+  ],
+  optimization: {
+    minimize: false
+  },
+  resolve: {
+    // extensions: [ '.ts', '.js' ],
+    fallback: {
+      vm: require.resolve("vm-browserify"),
+      // "stream": require.resolve("stream-browserify"),
+      "buffer": require.resolve("buffer")
+      // fs: false,
+      // "path": require.resolve("path-browserify"),
+      // "util": require.resolve("util/"),
+      // "assert": require.resolve("assert/"),
+    }
+  },
+  output: {
+    library: {
+      type: 'module'
+    },
+    filename: 'chain.browser.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  experiments: {
+    outputModule: true
+  }
+}, {
+  entry: './src/node.js',
+  plugins: [
+    // Work around for Buffer is undefined:
+    // https://github.com/webpack/changelog-v5/issues/10
+    // new webpack.ProvidePlugin({
+    //     Buffer: ['buffer', 'Buffer'],
+    // }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+
+    new webpack.ProvidePlugin({
+      WRTC_IMPORT: `globalThis.wrtc = {
+        RTCPeerConnection,
+        RTCSessionDescription,
+        RTCIceCandidate
+      }`
+    })
+  ],
+  optimization: {
+    minimize: false
+  },
+  resolve: {
+    // extensions: [ '.ts', '.js' ],
+    fallback: {
+      // "stream": require.resolve("stream-browserify"),
+      // "buffer": require.resolve("buffer"),
+      // fs: false,
+      // "path": require.resolve("path-browserify"),
+      // "util": require.resolve("util/"),
+      // "assert": require.resolve("assert/"),
+
+    }
+  },
+  output: {
+    library: {
+      type: 'module'
+    },
+    filename: 'node.browser.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  experiments: {
+    outputModule: true
+  }
+}];
