804bcc6097a8330586df38223cfd385e0928b569
vandeurenglenn <vandeurenglenn@gmail.com>
2022-05-15 18:21:17 +0200

    chain: include transactions in block

--- chain/src/protos/block.proto.js
+++ chain/src/protos/block.proto.js
@@ -4,13 +4,23 @@ message ValidatorMessage {
   required string reward = 2;
 }
 
+message Transaction {
+  required string hash = 1;
+  required uint64 timestamp = 2;
+  required string from = 3;
+  required string to = 4;
+  required uint64 nonce = 5;
+  required string method = 6;
+  repeated string params = 7;
+}
+
 message BlockMessage {
   required uint64 index = 1;
   required string previousHash = 3;
   required uint64 timestamp = 4;
   required uint64 reward = 5;
   required string fees = 6;
-  repeated string transactions = 7;
+  repeated Transaction transactions = 7;
   repeated ValidatorMessage validators = 8;
 }
 `
