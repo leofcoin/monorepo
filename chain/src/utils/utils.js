7c87efdf1736b73d33738ef94beebc437d798d6e
vandeurenglenn <vandeurenglenn@gmail.com>
2022-06-03 19:49:22 +0200

    Modulize more

--- chain/src/utils/utils.js
+++
@@ -1,7 +0,0 @@
-// import { promisify } from 'util'
-// import { writeFile, readFile } from 'fs'
-export { BigNumber } from "@ethersproject/bignumber";
-export { formatUnits, parseUnits } from '@ethersproject/units'
-
-// export const write = promisify(writeFile)
-// export const read = promisify(readFile)
