bdef652fa0ad7a8ede2e1f0bb9365fe4f50e3909
vandeurenglenn <vandeurenglenn@gmail.com>
2022-06-05 15:25:19 +0200

    messages: dynamic load

--- messages/src/messages/block.js
+++ messages/src/messages/block.js
@@ -1,14 +1,17 @@
-import protons from 'protons'
 import proto from './../protos/block.proto.js'
-import { FormatInterface as CodecFormat } from '@leofcoin/codec-format-interface'
+import { FormatInterface } from '@leofcoin/codec-format-interface'
 
-export default class BlockMessage extends CodecFormat {
+export default class BlockMessage extends FormatInterface {
   get keys() {
     return ['index', 'previousHash', 'timestamp', 'reward', 'fees', 'transactions', 'validators']
   }
 
+  get messageName() {
+    return 'BlockMessage'
+  }
+
   constructor(buffer) {
     const name = 'block-message'
-    super(buffer, protons(proto).BlockMessage, {name})
+    super(buffer, proto, {name})
   }
 }
