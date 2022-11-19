8177a74361533bb203ec06c0df0c7ac717d23e9c
vandeurenglenn <vandeurenglenn@gmail.com>
2022-11-19 18:53:04 +0100

    swarm: fix spawn star

--- peernet-swarm/spawn-stars.js
+++ peernet-swarm/spawn-stars.js
@@ -1,11 +1,12 @@
 const {spawn} = require('child_process')
 
 const networks = [{
-  name: 'leofcoin:peach',
+  network: 'leofcoin:peach',
+  networkVersion: 'peach',
   port: 44444
 }]
 
 
-for (const [port, name] of networks) {
-  spawn('./spawn-star.js', ['--port', port, '--network', name])
+for (const {port, name} of networks) {
+  spawn('node', ['./spawn-star.js', '--port', port, '--network', network], {cwd: __dirname})
 }
