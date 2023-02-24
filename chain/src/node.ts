f81b05bff433b57ee831ca7efd2986901fdea2b5
Glenn Vandeuren <vandeurenglenn@gmail.com>
2023-02-24 17:25:15 +0100

    chain: pass to node

--- chain/src/node.ts
+++ chain/src/node.ts
@@ -4,8 +4,8 @@ import nodeConfig from '@leofcoin/lib/node-config';
 import networks from '@leofcoin/networks';
 
 export default class Node {
-  constructor() {
-    return this._init()
+  constructor(config, password: string) {
+    return this._init(config, password)
   }
 
   async _init(config = {
@@ -13,8 +13,8 @@ export default class Node {
     networkName: 'leofcoin:peach',
     networkVersion: 'peach',
     stars: networks.leofcoin.peach.stars
-  }): Promise<this> {
-    globalThis.Peernet ? await new globalThis.Peernet(config) : await new Peernet(config)
+  }, password: string): Promise<this> {
+    globalThis.Peernet ? await new globalThis.Peernet(config, password) : await new Peernet(config, password)
     await nodeConfig(config)
 
     return this
