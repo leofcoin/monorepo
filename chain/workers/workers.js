d814ebca1f339a7d3a4931ff9b4bd75b59b3be47
Glenn Vandeuren <vandeurenglenn@gmail.com>
2023-02-28 21:08:31 +0100

    chain: cleanup workers

--- chain/workers/workers.js
+++
@@ -1,9 +0,0 @@
-import * as transactionWorker from './transaction-worker.js'
-import * as blockWorker from './block-worker.js'
-import * as poolWorker from './pool-worker.js'
-import * as machineWorker from './machine-worker.js'
-
-export const TransactionWorker = transactionWorker
-export const BlockWorker = blockWorker
-export const PoolWorker = poolWorker
-export const MachineWorker = machineWorker
