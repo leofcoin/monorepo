302a48d82434d6d8b34877471713c2b70b100794
Glenn Vandeuren <vandeurenglenn@gmail.com>
2023-03-04 11:36:49 +0100

    swarm: update test

--- peernet-swarm/test/index.js
+++ peernet-swarm/test/index.js
@@ -1,6 +1,15 @@
-const Client = require('./../dist/commonjs/client');
-const Server = require('./../dist/commonjs/server');
+import Client from './../exports/client.js'
+import Server from './../exports/server.js'
 
 globalThis.DEBUG = true
-const server = new Server().then(() => new Client())
+
+pubsub.subscribe('peer:connected', (peer) => {
+  console.log(peer);
+  peer.send('hi')
+})
+
+const server = await new Server()
 pubsub.subscribe('peer:data', (data) => console.log(new TextDecoder().decode(data.data)))
+
+await new Client('a', 'peach')
+const client = await new Client('b', 'peach')
