846bf22dffc8d181f70db9ad4cf6eec12eb91863
Glenn Vandeuren <vandeurenglenn@gmail.com>
2023-03-04 11:38:59 +0100

    chain: upgrade peernet

--- chain/rollup.config.js
+++ chain/rollup.config.js
@@ -1,6 +1,8 @@
 import json from '@rollup/plugin-json'
 import typescript from '@rollup/plugin-typescript'
+import nodeResolve from '@rollup/plugin-node-resolve'
 import tsConfig from './tsconfig.json' assert { type: 'json'}
+import commonjs from '@rollup/plugin-commonjs'
 
 export default [{
   input: ['./src/chain.ts', './src/node.ts'],
@@ -11,5 +13,19 @@ export default [{
   plugins: [
     json(),
     typescript(tsConfig)
+  ]
+}, {
+  input: ['./src/chain.ts', './src/node-browser.ts', './node_modules/@leofcoin/storage/exports/browser-store.js'],
+  output: {
+    dir: './exports/browser',
+    format: 'es'
+  },
+  plugins: [
+    json(),
+    nodeResolve({
+      mainFields: ['module', 'browser']
+    }),
+    commonjs({exclude: ['simple-peer', './simple-peer.js']}),
+    typescript({...tsConfig, compilerOptions: {'outDir': './exports/browser', 'declaration': false, 'declarationDir': './exports/browser'}})
   ]
 }]
--- chain/src/node-browser.ts
+++ chain/src/node-browser.ts
@@ -1,5 +1,5 @@
 // import config from './config/config'
-import Peernet from '@leofcoin/peernet'
+import Peernet from '@leofcoin/peernet/browser'
 import nodeConfig from '@leofcoin/lib/node-config';
 import networks from '@leofcoin/networks';
 
@@ -18,7 +18,7 @@ export default class Node {
   }, password: string): Promise<this> {
     this.#node = globalThis.Peernet ? await new globalThis.Peernet(config, password) : await new Peernet(config, password)
     await nodeConfig(config)
-    
+
     globalThis.pubsub.subscribe('chain:ready', () => {
       if (!this.#node.autoStart) this.#node.start()
     })
