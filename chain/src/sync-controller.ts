2f2e4573abd336b5909b0c56b3bd45bed7216241
Glenn Vandeuren <vandeurenglenn@gmail.com>
2023-05-07 13:35:55 +0200

    chai: initial sync-controller

---
+++ chain/src/sync-controller.ts
@@ -0,0 +1,44 @@
+export default class SyncController {
+  #busy: boolean
+  #loading: boolean
+  #resolving: boolean
+  #fullyResolved: boolean
+  #fullyLoaded: boolean
+
+  get busy() {
+    return this.#busy
+  }
+
+  get loading() {
+    return this.#loading
+  }
+
+  get resolving() {
+    return this.#resolving
+  }
+
+  get fullyResolved() {
+    return this.#fullyResolved
+  }
+
+  get fullyLoaded() {
+    return this.#fullyLoaded
+  }
+
+  constructor() {
+    this.#busy = false
+    this.#loading = false
+    this.#resolving = false
+    this.#fullyResolved = false
+    this.#fullyLoaded = false
+  }
+
+
+  /**
+   * Resolves/rejects a promise or rejects on timeout
+   */
+  resolve() {
+    
+  }
+
+}
