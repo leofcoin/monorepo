cb161c0b67a5f62f25e98f614b79ab9ed757bcca
Glenn Vandeuren <vandeurenglenn@gmail.com>
2023-09-21 20:02:35 +0200

    networks: start switch to unified stars

--- packages/networks/rollup.config.js
+++ packages/networks/rollup.config.js
@@ -1,5 +1,4 @@
 import typescript from '@rollup/plugin-typescript'
-import defaultToJSON from 'rollup-plugin-default-to-json'
 
 export default [{
   input: './src/networks.ts',
@@ -7,5 +6,5 @@ export default [{
     dir: 'exports',
     format: 'es'
   }],
-  plugins: [defaultToJSON(), typescript()]
+  plugins: [typescript()]
 }]
--- packages/networks/src/networks.ts
+++ packages/networks/src/networks.ts
@@ -1,13 +1,25 @@
+// todo only one star needed, no need to have one for each network
+// unless we change anything to the star protocoll
+// version diferences should be handled in the chain
+// maybe a good way to handle could be in p2pt-swarm
+
 export default {
   leofcoin: {
     mainnet: {
-      port: 33333,
-      stars: ['wss://mainnet.leofcoin.org'] // todo webrtc and bittorent stars
+      // ports don't really matter since it is favorable to have it begind a ngninx proxy but if we change something to the proto it's easier maybe?
+      port: 44444,
+      // todo a versionhash would be nice to have as a double check?
+      versionHash: '0',
+      // a short description identifying the version
+      description: 'Main net current version',
+      stars: ['wss://star.leofcoin.org'] // todo webrtc and bittorent stars
     }, 
     
     peach: {
       port: 44444,
-      stars: ['wss://peach.leofcoin.org'] // todo webrtc and bittorent stars
+      description: 'Main testnet: latest step before merging into main',
+      versionHash: '1',
+      stars: ['wss://star.leofcoin.org'] // todo webrtc and bittorent stars
     }
   }
 }
