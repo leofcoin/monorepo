dcff4f9ff23415400b13d91ad87b1aef32f8fb5f
Glenn Vandeuren <vandeurenglenn@gmail.com>
2023-10-22 16:30:28 +0200

    errors:v 1.0.2

--- packages/errors/src/errors.ts
+++ packages/errors/src/errors.ts
@@ -18,5 +18,24 @@ export class SyncError extends LeofcoinError {
   name: string = 'SyncError'
 }
 
+export class InvalidBlockError extends LeofcoinError {
+  name: string = 'InvalidBlockError'
+}
+
+export class ExecutionError extends LeofcoinError {
+  name: string = 'ExecutionError'
+}
+
+export class ContractDeploymentError extends LeofcoinError {
+  name: string = 'ContractDeploymentError'
+}
+
 export const isResolveError = (error: LeofcoinError) => error.name === 'ResolveError'
+
 export const isSyncError = (error: LeofcoinError) => error.name === 'SyncError'
+
+export const isInvalidBlockError = (error: LeofcoinError) => error.name === 'InvalidBlockError'
+
+export const isExecutionError = (error: LeofcoinError) => error.name === 'ExecutionError'
+
+export const isContractDeploymentError = (error: LeofcoinError) => error.name === 'ContractDeploymentError'
