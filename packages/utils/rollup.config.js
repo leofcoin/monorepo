62b5bafcd8cba331d0b4e0c916adc708c1284b36
VandeurenGlenn <8685280+VandeurenGlenn@users.noreply.github.com>
2024-09-30 19:48:58 +0200

    utils: update rollup config

--- packages/utils/rollup.config.js
+++ packages/utils/rollup.config.js
@@ -1,7 +1,5 @@
-import json from '@rollup/plugin-json'
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 export default [
   {
     input: ['./src/utils.ts'],
@@ -9,24 +7,6 @@ export default [
       dir: './exports',
       format: 'es'
     },
-    plugins: [json(), typescript()]
-  },
-  {
-    input: ['./src/utils.ts'],
-    output: {
-      dir: './exports/browser',
-      format: 'es'
-    },
-    plugins: [
-      json(),
-      typescript({ compilerOptions: { declaration: false, outDir: 'exports/browser' } }),
-      resolve({
-        browser: true,
-        preferBuiltins: false
-      }),
-      commonjs({
-        include: ['node_modules/bn.js/lib/bn.js', '../../node_modules/bn.js/lib/bn.js']
-      })
-    ]
+    plugins: [typescript(), nodeResolve()]
   }
 ]
