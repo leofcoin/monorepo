4b29a1f5788469b7cd6e2f3738ec878323f5821e
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2025-07-06 18:36:08 +0200

    Add flush script to clear node stores
    Introduces scripts/flush.js to clear various global stores related to accounts, chain, blocks, transactions, state, and transaction pool. This utility can be used to reset the node's persistent data.

---
+++ scripts/flush.js
@@ -0,0 +1,15 @@
+import { unlink } from 'fs/promises'
+import inquirer from 'inquirer'
+import { nodeConfig } from '@leofcoin/lib'
+import { glob } from 'fs/promises'
+
+const Node = (await import('../packages/chain/exports/node.js')).default
+const node = await new Node({ network: 'leofcoin:peach', networkVersion: 'peach' })
+const config = await nodeConfig()
+
+await globalThis.accountsStore.clear()
+await globalThis.chainStore.clear()
+await globalThis.blockStore.clear()
+await globalThis.transactionStore.clear()
+await globalThis.stateStore.clear()
+await globalThis.transactionPoolStore.clear()
