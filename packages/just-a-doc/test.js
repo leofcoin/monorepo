49c6ca2071f38b99c4d14be2f576554734420952
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2026-07-19 20:58:56 +0200

    Make documentation tests clean-checkout safe

--- packages/just-a-doc/test.js
+++ packages/just-a-doc/test.js
@@ -1,7 +1,21 @@
-import parse from './dist/index.js'
+import parse from './src/index.ts'
+import assert from 'node:assert/strict'
+import { access, mkdtemp, rm } from 'node:fs/promises'
+import { tmpdir } from 'node:os'
+import { join } from 'node:path'
+import test from 'node:test'
 
-parse({
-  input: ['./*.js', '!test.js', '!rollup.config.js', '!tsconfig.js'],
-  output: 'docs',
-  readme: './README.md'
+test('generates documentation into an isolated output directory', async () => {
+  const output = await mkdtemp(join(tmpdir(), 'doc-it-'))
+  try {
+    await parse({
+      input: ['./testClass.js'],
+      output,
+      readme: './README.md'
+    })
+    await assert.doesNotReject(access(join(output, 'index.html')))
+    await assert.doesNotReject(access(join(output, 'testClass.js')))
+  } finally {
+    await rm(output, { recursive: true, force: true })
+  }
 })
